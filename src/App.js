13027360f3bdc4379071cbaf4edb7ca074597ea0
Zomebie <jello.cho@key-inside.com>
2020-06-30 23:06:31 +0900

    lifecycle of class component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react'
-import Validation from './components/Validation'
+import React, { Component } from 'react'
+import LifeCycle from './components/LifeCycle'
 
-function App() {
-	return <Validation />
+function getRandomColor() {
+	return '#' + Math.floor(Math.random() * 16777215).toString(16)
+}
+
+class App extends Component {
+	state = {
+		color: '#000000',
+	}
+
+	handleClick = () => {
+		this.setState({
+			color: getRandomColor(),
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<button onClick={this.handleClick}>랜덤 색상</button>
+				<LifeCycle color={this.state.color} />
+			</div>
+		)
+	}
 }
 
 export default App
